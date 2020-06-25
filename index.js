624301678967792b87285e0e5f91608bb9d7eee4
Justin Ochoa <just.och92@gmail.com>
2020-06-25 12:39:09 -0400

    loops

--- index.js
+++ index.js
@@ -74,3 +74,96 @@ if (ingredients.includes("flour")) {
 let letters = ["T", "C", "E", "P", "S", "E", "R"];
 letters.reverse().join();
 console.log(letters);
+
+// objects
+const numbers = {
+  color: "red",
+  100: "one hundred",
+  16: "sixteen",
+  "46 trombone": "Great Song!",
+};
+console.log(numbers[100]);
+console.log(numbers.color);
+
+const palette = {
+  red: "#eb4d4b",
+  yellow: "#f9ca24",
+  blue: "#30336b",
+};
+console.log(palette["blue"]);
+
+// adds to an empty Object
+
+const userReviews = {};
+userReviews.lilJohn = 4.5;
+console.log(userReviews);
+
+// adds to an Object
+userReviews.lilJohn += 2;
+console.log(userReviews);
+
+// js loops
+for (i = 1; i <= 10; i++) {
+  console.log("hello");
+}
+
+const animals = ["lions", "tigers", "bears"];
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Outer Loop", i);
+  for (let j = 10; j >= 0; j -= 2) {
+    console.log("   Inner Loop", j);
+  }
+}
+
+// while loop
+let target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
+while (guess != target) {
+  console.log(`Target: ${target} Guess: ${guess}`);
+  guess = Math.floor(Math.random() * 10);
+}
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log("Congrats you Win!");
+
+// for of loop
+let subreddits = ["soccer", "popheads", "cringe", "books"];
+for (i = 0; i <= subreddits.length; i++) {
+  console.log(subreddits[i]);
+}
+for (let sub of subreddits) {
+  console.log(sub);
+}
+
+// for of loops with Object
+const movieReviews = {
+  Arrival: 9.5,
+  Alein: 9,
+  Amelie: 8,
+  "In Bruges": 9,
+  Amadeus: 10,
+  "Kill Bill": 8,
+  "Little miss Sunshine": 8.5,
+  Coraline: 7.5,
+};
+// You cannot Iterate through an object so you have to use object.keys
+
+for (movie of Object.keys(movieReviews)) {
+  console.log(movie, movieReviews[movie]);
+}
+
+// for in loop
+
+const jeopardyWinnings = {
+  regularPlay: 2522700,
+  watsonChallenge: 300000,
+  tournamentOfChampions: 500000,
+  battleOfTheDecades: 10000,
+};
+for (prop in jeopardyWinnings) {
+  console.log(prop);
+  console.log(jeopardyWinnings[prop]);
+}
