e0083975c30b9418b8f6b439c93e786abdcc0c33
Justin Ochoa <just.och92@gmail.com>
2020-07-28 10:37:33 -0400

    promises

--- callbacks/index.js
+++ callbacks/index.js
@@ -57,10 +57,151 @@ const moveX = (element, amount, delay, callback) => {
     }, delay);
   }
 };
-moveX(btn, 100, 1000, function () {
-  moveX(btn, 100, 1000, function () {
-    moveX(btn, 100, 1000, function () {
-      moveX(btn, 10000, 1000);
-    });
+// moveX(btn, 100, 1000, function () {
+//   moveX(btn, 100, 1000, function () {
+//     moveX(btn, 100, 1000, function () {
+//       moveX(btn, 10000, 1000);
+//     });
+//   });
+// });
+
+// using promises to move button
+
+const moveX2 = (element, amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const boundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const elLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > boundary) {
+        reject();
+      } else {
+        element.style.transform = element.style.transform = `translateX(${
+          elLeft + amount
+        }px)`;
+        resolve();
+      }
+    }, delay);
   });
+};
+moveX2(btn, 300, 1000)
+  .then(() => moveX2(btn, 300, 1000))
+  .then(() => moveX2(btn, 300, 1000))
+  .then(() => moveX2(btn, 300, 1000))
+  .then(() => moveX2(btn, 300, 1000))
+  .then(() => moveX2(btn, 300, 1000))
+  .catch(() => {
+    console.log("Out Of Space");
+  });
+// promises
+
+const willGetYouADog = new Promise((resolve, reject) => {
+  const rand = Math.random();
+  if (rand < 0.5) {
+    resolve();
+  } else {
+    reject();
+  }
 });
+willGetYouADog.then(() => {
+  console.log(`Yay we got a dog!!`);
+});
+willGetYouADog.catch(() => {
+  console.log("No Dog :(");
+});
+
+// promises in a function
+
+function makeDogPromise() {
+  return new Promise((resolve, reject) => {
+    const rand = Math.random();
+    if (rand < 0.5) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
+makeDogPromise()
+  .then(() => {
+    console.log(`Yay we got a dog!!`);
+  })
+  .catch(() => {
+    console.log("No Dog :(");
+  });
+
+// fake requests
+
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const pages = {
+        "/users": [
+          { id: 1, username: "Bilbo" },
+          { id: 5, username: "Esmerelda" },
+        ],
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321,
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu",
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet",
+        },
+        "/about": "This is the about page!",
+      };
+      const data = pages[url];
+      if (data) {
+        resolve({ status: 200, data }); //resolve with a value!
+      } else {
+        reject({ status: 404 }); //reject with a value!
+      }
+    }, 1000);
+  });
+};
+fakeRequest("/users")
+  .then((res) => {
+    console.log(res);
+    const id = res.data[0].id;
+    return fakeRequest(`/users/${id}`);
+  })
+  .then((res) => {
+    console.log(res);
+    const postId = res.data.topPostId;
+    return fakeRequest(`/posts/${postId}`);
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log("OH NO!", err);
+  });
+
+// ATTEMPT 2 (with error to show CATCH)
+
+// fakeRequest('/users')
+// 	.then((res) => {
+// 		console.log(res);
+// 		const id = res.data[0].id;
+// 		return fakeRequest(`/useALSKDJrs/${id}`); //INVALID URL, CATCH WILL RUN!
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 		const postId = res.data.topPostId;
+// 		return fakeRequest(`/posts/${postId}`);
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO!', err);
+// 	})
